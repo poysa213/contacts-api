947681ff3291b7679344ed717b125ab7644652e0
poysa213 <y.hanaia@esi-sba.dz>
2022-12-21 05:42:34 +0100

    edit requirements

--- api/settings.py
+++ api/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'api.urls'
