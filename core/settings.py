a4fc6af64310fd2bd54679059bcb43e85ba54d83
Youcef Hanaia <y.hanaia@esi-sba.dz>
2023-04-19 23:28:52 +0100

    change the return type

--- core/settings.py
+++ core/settings.py
@@ -148,7 +148,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer',
+        # 'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
